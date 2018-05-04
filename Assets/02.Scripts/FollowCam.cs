dc85d80cdd112ef0b11ecb680a6ae52858126c49
GongYi <982385563@qq.com>
2018-05-04 22:59:54 +0800

    Unity_TPS_first commit

---
+++ Assets/02.Scripts/FollowCam.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+
+    public Transform targetTr;       //追踪的游戏对象的Transform
+    public float dist = 10.0f;       //摄像机与目标的水平距离
+    public float height = 3.0f;      //摄像机高度
+    public float dampTrace = 20.0f;  //实现平滑追踪的变量
+
+    //摄像机的Transform
+    private Transform tr;
+
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //目标停止移动后才调用，可以避免相机抖动
+    void LateUpdate()
+    {
+        //保存相机在目标后方dist，高height的位置
+        //Lerp函数进行插值运算，达到一个平滑的效果
+        tr.position = Vector3.Lerp(tr.position, 
+                                   targetTr.position - (targetTr.forward * dist) + (Vector3.up * height), 
+                                   Time.deltaTime * dampTrace);
+        //使相机朝向目标
+        tr.LookAt(targetTr.position);
+    }
+}
---
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+
+    private float h = 0.0f;
+    private float v = 0.0f;
+    private Transform tr;
+    //移动速度
+    public float moveSpeed = 10.0f;
+    //旋转速度
+    public float rotSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        //向量计算得到一个运动的方向向量
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        //Translate( 移动方向 * 速度 * 位移值 * Time.deltaTime, 基础坐标)
+        tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
+        //以Vector3.up 轴为基准，以rotSpeed速度旋转
+        tr.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
+
+	}
+}
